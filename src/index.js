5931585a0a65723080534cb0b4e1f6cda64fbd14
x74z <166341818+x74z@users.noreply.github.com>
2024-11-29 16:52:44 +0000

    Add todo comments

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import { createDefaultTodo, loadDefaultTodos } from "./load-default-project";
 import { createTodayTodo, loadTodayTodos } from "./load-today-todos";
 import { createTodo, loadAllTodos } from "./load-all-todos";
 
-// Code that will change between projects
+// TODO i should do one of these things:
+// Make an actual dialog for adding todos, figure out how to get the dates, make it readable, unlike the library project.
+//
 
 // Loading default project
 document.querySelector("#default-todos").addEventListener("pointerdown", loadDefaultTodos);
