3128f69f7e6f10612481cbaf21548899326d6089
x74z <166341818+x74z@users.noreply.github.com>
2024-12-18 21:09:27 +0000

    Make default todo creation date today

--- src/create-todo-dialog.js
+++ src/create-todo-dialog.js
@@ -1,3 +1,5 @@
+import { getTodaysDateInDatetimelocal } from "./format-date-for-todos";
+
 // This function is just to add the close functionality
 (function () {
   const dialog = document.querySelector(".js-add-todo-dialog");
@@ -21,9 +23,11 @@ export function showTodoCreationDialog(projectObject) {
   // When that button gets clicker, the parameter of the function(That creates a new todo, specified by the caller)
   // will create a new todo, hopefully
   const dialog = document.querySelector(".js-add-todo-dialog");
-  dialog.showModal();
-
   const form = document.querySelector(".js-add-todo-dialog-form");
+
+  // Set the default date to today.
+  form.date.value = getTodaysDateInDatetimelocal();
+  dialog.showModal();
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -45,12 +49,17 @@ export function showTodoCreationDialog(projectObject) {
   form.addEventListener("submit", handleSubmit);
 }
 
+// The difference between this function and the one for projects is that when
+// a new dialog is open, it calls the createTodo function without a project, this way
+// the todo gets added to the Todo.todos right away.
 export function showTodoCreationDialogForNonProjects(createTodoFunction){
 
   const dialog = document.querySelector(".js-add-todo-dialog");
+  const form = document.querySelector(".js-add-todo-dialog-form");
+
+  form.date.value = getTodaysDateInDatetimelocal();
   dialog.showModal();
 
-  const form = document.querySelector(".js-add-todo-dialog-form");
   function handleSubmit(e) {
     e.preventDefault();
     const todoTitle = form.title.value;
